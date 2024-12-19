b253a77e05188b5641db36d9bdaf3645615af01d
ibrahim-sisar-test <ibrahim.sisar@hotmail.com>
2024-12-19 12:22:46 +0200

    Create main.c
    add c file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+  char * a;
+  char * b;
+  printf("name: ");
+  scanf("%s",&a);
+  printf("age: ");
+  scanf("%s",&b);
+  printf("name: %s | age: %b\n",a,b);
+}
